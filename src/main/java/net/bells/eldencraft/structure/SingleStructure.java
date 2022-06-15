194494f608d4c4fafa09d9a33256bf84eb5869a8
Josef Eklund <bjorn.josef.eklund@gmail.com>
2022-06-15 11:24:02 +0200

    rename

--- src/main/java/net/bells/eldencraft/structure/SingleStructure.java
+++ src/main/java/net/bells/eldencraft/structure/SingleStructure.java
@@ -32,8 +32,8 @@ import org.apache.logging.log4j.Level;
 
 import java.util.List;
 
-public class TowerStructure extends StructureFeature<NoneFeatureConfiguration> {
-    public TowerStructure(Codec<NoneFeatureConfiguration> codec) {
+public class SingleStructure extends StructureFeature<NoneFeatureConfiguration> {
+    public SingleStructure(Codec<NoneFeatureConfiguration> codec) {
         super(codec);
     }
 
@@ -43,7 +43,7 @@ public class TowerStructure extends StructureFeature<NoneFeatureConfiguration> {
      */
     @Override
     public StructureStartFactory<NoneFeatureConfiguration> getStartFactory() {
-        return TowerStructure.Start::new;
+        return SingleStructure.Start::new;
     }
 
 
@@ -205,8 +205,62 @@ public class TowerStructure extends StructureFeature<NoneFeatureConfiguration> {
                     true,  // Place at heightmap (top land). Set this to false for structure to be place at the passed in blockpos's Y value instead.
                     // Definitely keep this false when placing structures in the nether as otherwise, heightmap placing will put the structure on the Bedrock roof.
                     heightLimitView);
+            JigsawPlacement.addPieces(
+                    dynamicRegistryAccess,
+                    new JigsawConfiguration(() -> dynamicRegistryAccess.registryOrThrow(Registry.TEMPLATE_POOL_REGISTRY)
+                            // The path to the starting Template Pool JSON file to read.
+                            //
+                            // Note, this is "structure_tutorial:run_down_house/start_pool" which means
+                            // the game will automatically look into the following path for the template pool:
+                            // "resources/data/structure_tutorial/worldgen/template_pool/run_down_house/start_pool.json"
+                            // This is why your pool files must be in "data/<modid>/worldgen/template_pool/<the path to the pool here>"
+                            // because the game automatically will check in worldgen/template_pool for the pools.
+                            .get(new ResourceLocation(EldenCraft.MOD_ID, "large_ruins/start_pool")),
 
+                            // How many pieces outward from center can a recursive jigsaw structure spawn.
+                            // Our structure is only 1 piece outward and isn't recursive so any value of 1 or more doesn't change anything.
+                            // However, I recommend you keep this a decent value like 10 so people can use datapacks to add additional pieces to your structure easily.
+                            // But don't make it too large for recursive structures like villages or you'll crash server due to hundreds of pieces attempting to generate!
+                            10),
+                    PoolElementStructurePiece::new,
+                    chunkGenerator,
+                    structureManager,
+                    structureBlockPos, // Position of the structure. Y value is ignored if last boolean parameter is set to true.
+                    this, // The structure class to populate its pieces field with the jigsaw pieces after this method is ran.
+                    this.random,
+                    false, // Special boundary adjustments for villages. It's... hard to explain. Keep this false and make your pieces not be partially intersecting.
+                    // Either not intersecting or fully contained will make children pieces spawn just fine. It's easier that way.
+                    true,  // Place at heightmap (top land). Set this to false for structure to be place at the passed in blockpos's Y value instead.
+                    // Definitely keep this false when placing structures in the nether as otherwise, heightmap placing will put the structure on the Bedrock roof.
+                    heightLimitView);
+            JigsawPlacement.addPieces(
+                    dynamicRegistryAccess,
+                    new JigsawConfiguration(() -> dynamicRegistryAccess.registryOrThrow(Registry.TEMPLATE_POOL_REGISTRY)
+                            // The path to the starting Template Pool JSON file to read.
+                            //
+                            // Note, this is "structure_tutorial:run_down_house/start_pool" which means
+                            // the game will automatically look into the following path for the template pool:
+                            // "resources/data/structure_tutorial/worldgen/template_pool/run_down_house/start_pool.json"
+                            // This is why your pool files must be in "data/<modid>/worldgen/template_pool/<the path to the pool here>"
+                            // because the game automatically will check in worldgen/template_pool for the pools.
+                            .get(new ResourceLocation(EldenCraft.MOD_ID, "minor_erdtree/start_pool")),
 
+                            // How many pieces outward from center can a recursive jigsaw structure spawn.
+                            // Our structure is only 1 piece outward and isn't recursive so any value of 1 or more doesn't change anything.
+                            // However, I recommend you keep this a decent value like 10 so people can use datapacks to add additional pieces to your structure easily.
+                            // But don't make it too large for recursive structures like villages or you'll crash server due to hundreds of pieces attempting to generate!
+                            10),
+                    PoolElementStructurePiece::new,
+                    chunkGenerator,
+                    structureManager,
+                    structureBlockPos, // Position of the structure. Y value is ignored if last boolean parameter is set to true.
+                    this, // The structure class to populate its pieces field with the jigsaw pieces after this method is ran.
+                    this.random,
+                    false, // Special boundary adjustments for villages. It's... hard to explain. Keep this false and make your pieces not be partially intersecting.
+                    // Either not intersecting or fully contained will make children pieces spawn just fine. It's easier that way.
+                    true,  // Place at heightmap (top land). Set this to false for structure to be place at the passed in blockpos's Y value instead.
+                    // Definitely keep this false when placing structures in the nether as otherwise, heightmap placing will put the structure on the Bedrock roof.
+                    heightLimitView);
             // **THE FOLLOWING LINE IS OPTIONAL**
             //
             // Right here, you can do interesting stuff with the pieces in this.pieces such as offset the
