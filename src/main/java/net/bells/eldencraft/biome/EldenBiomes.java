63522af1e0896e99083f7421b84ef7246eaf974c
Josef Eklund <bjorn.josef.eklund@gmail.com>
2022-06-15 11:23:29 +0200

    biome wip

--- src/main/java/net/bells/eldencraft/biome/EldenBiomes.java
+++ src/main/java/net/bells/eldencraft/biome/EldenBiomes.java
@@ -5,10 +5,17 @@ import net.bells.eldencraft.EldenCraft;
 import net.bells.eldencraft.gen.ModConfiguredFeatures;
 import net.minecraft.data.worldgen.BiomeDefaultFeatures;
 import net.minecraft.data.worldgen.Features;
+import net.minecraft.util.valueproviders.ConstantInt;
 import net.minecraft.world.level.biome.*;
 import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.RandomFeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
+import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
+import net.minecraft.world.level.levelgen.feature.stateproviders.SimpleStateProvider;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
 import net.minecraft.world.level.levelgen.placement.FeatureDecorator;
 import net.minecraft.world.level.levelgen.placement.FrequencyWithExtraChanceDecoratorConfiguration;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -23,8 +30,12 @@ public class EldenBiomes extends Biomes {
 
     public static final RegistryObject<Biome> ELDEN_LAND = BIOMES.register("elden_land",
             EldenBiomes::createEldenLand);
+    public static final RegistryObject<Biome> ALTUS_PLATEAU = BIOMES.register("altus_plateau",
+            EldenBiomes::createAltusPlateau);
 
 
+
+    //public static final ConfiguredFeature<?, ?> LIMGRAVE_TREES = register("treeman", Feature.RANDOM_SELECTOR.configured(new RandomFeatureConfiguration(ImmutableList.of(Features.FANCY_OAK.weighted(0.1F)), Features.OAK)).decorated(Features.Decorators.HEIGHTMAP_WITH_TREE_THRESHOLD_SQUARED).decorated(FeatureDecorator.COUNT_EXTRA.configured(new FrequencyWithExtraChanceDecoratorConfiguration(0, 0.1F, 1))));
     private static Biome createEldenLand() {
         MobSpawnSettings.Builder spawnSettings = new MobSpawnSettings.Builder();
         BiomeDefaultFeatures.farmAnimals(spawnSettings);
@@ -39,12 +50,13 @@ public class EldenBiomes extends Biomes {
         BiomeDefaultFeatures.addDefaultExtraVegetation(generationSettings);
         BiomeDefaultFeatures.addDefaultLakes(generationSettings);
         BiomeDefaultFeatures.addMossyStoneBlock(generationSettings);
-        //BiomeDefaultFeatures.addWaterTrees(generationSettings);
-
-        generationSettings.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, Feature.RANDOM_SELECTOR.configured(new RandomFeatureConfiguration(ImmutableList.of(ModConfiguredFeatures.LIMGRAVE_OAK.weighted(0.025641026F), ModConfiguredFeatures.LIMGRAVE_OAK.weighted(0.30769232F), ModConfiguredFeatures.LIMGRAVE_OAK.weighted(0.33333334F)), Features.SPRUCE)).decorated(Features.Decorators.HEIGHTMAP_WITH_TREE_THRESHOLD_SQUARED).decorated(FeatureDecorator.COUNT_EXTRA.configured(new FrequencyWithExtraChanceDecoratorConfiguration(10, 0.1F, 1))));;
+        BiomeDefaultFeatures.addWaterTrees(generationSettings);
         //BiomeDefaultFeatures.addBambooVegetation(generationSettings);
 
-        generationSettings.addFeature(GenerationStep.Decoration.LAKES, Features.LAKE_LAVA);
+        BiomeDefaultFeatures.addPlainVegetation(generationSettings);
+        BiomeDefaultFeatures.commonSpawns(spawnSettings);
+
+        BiomeDefaultFeatures.addForestFlowers(generationSettings);
 
         BiomeDefaultFeatures.addDefaultOres(generationSettings);
         BiomeDefaultFeatures.addDefaultSoftDisks(generationSettings);
@@ -76,7 +88,58 @@ public class EldenBiomes extends Biomes {
                         .build()).mobSpawnSettings(spawnSettings.build())
                 .generationSettings(generationSettings.build()).build();
     }
+    private static Biome createAltusPlateau() {
+        MobSpawnSettings.Builder spawnSettings = new MobSpawnSettings.Builder();
+        BiomeDefaultFeatures.farmAnimals(spawnSettings);
+        BiomeDefaultFeatures.monsters(spawnSettings, 95, 5, 100);
 
+        BiomeGenerationSettings.Builder generationSettings = new BiomeGenerationSettings.Builder();
+        generationSettings.surfaceBuilder(ModSurfaceConfigs.ELDEN_SURFACE_BUILDER);
+
+        BiomeDefaultFeatures.addDefaultUndergroundVariety(generationSettings);
+        BiomeDefaultFeatures.addDefaultCarvers(generationSettings);
+        BiomeDefaultFeatures.addPlainGrass(generationSettings);
+        BiomeDefaultFeatures.addDefaultExtraVegetation(generationSettings);
+        BiomeDefaultFeatures.addDefaultLakes(generationSettings);
+        BiomeDefaultFeatures.addMossyStoneBlock(generationSettings);
+        BiomeDefaultFeatures.addWaterTrees(generationSettings);
+        //BiomeDefaultFeatures.addBambooVegetation(generationSettings);
+
+        BiomeDefaultFeatures.addPlainVegetation(generationSettings);
+        BiomeDefaultFeatures.commonSpawns(spawnSettings);
+
+        BiomeDefaultFeatures.addForestFlowers(generationSettings);
+
+        BiomeDefaultFeatures.addDefaultOres(generationSettings);
+        BiomeDefaultFeatures.addDefaultSoftDisks(generationSettings);
+        BiomeDefaultFeatures.addDefaultSprings(generationSettings);
+        //generationSettings.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION,  ModConfiguredFeatures.LIMGRAVE_OAK
+        //       .decorated(Features.Decorators.HEIGHTMAP_WITH_TREE_THRESHOLD_SQUARED)
+        //     .decorated(FeatureDecorator.COUNT_EXTRA
+        //           .configured(new FrequencyWithExtraChanceDecoratorConfiguration(
+        //                 2, 0.8f, 1))));
+
+
+        int skycolor = 0x435e5c;
+        int fogcolor = 0xadc4c2;
+        int folcolor = 0xf5cc27;
+        return (new Biome.BiomeBuilder()).precipitation(Biome.Precipitation.RAIN).biomeCategory(Biome.BiomeCategory.NONE)
+                .depth(0.125F).scale(1.0F).temperature(0.8F).downfall(0.4F)
+                .specialEffects((new BiomeSpecialEffects.Builder())
+                        // COLORS
+                        // WATER
+                        .waterColor(0x9bbdbf)
+                        .waterFogColor(0x9bbdbf)
+                        // FOG
+                        .fogColor(fogcolor)
+                        .skyColor(skycolor)
+                        // GRASS + LEAVES
+                        .grassColorOverride(0x93b565)
+                        .foliageColorOverride(folcolor)
+
+                        .build()).mobSpawnSettings(spawnSettings.build())
+                .generationSettings(generationSettings.build()).build();
+    }
     public static void register(IEventBus eventBus)
     {
         BIOMES.register(eventBus);
